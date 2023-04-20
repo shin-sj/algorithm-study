8317e024f0be0b0c80562b0a4ef9423327fb9c10
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-04-20 10:55:53 +0900

    [level 2] Title: 소수 찾기, Time: 0.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,20 +1,21 @@
 from itertools import permutations
-def ch(n) :
+def ch(n): 
     if n < 2 :
         return False
-    for i in range(2, int(n**0.5) + 1) :
-        if n % i == 0 :
+    for i in range(2, int(n ** 0.5) + 1) :
+        if n % i == 0:
             return False
     return True
 
 def solution(numbers):
-    answer = []
     tmp = []
-    for i in range(1, len(numbers)+1) :
+    for i in range(1, len(numbers) + 1 ): 
         tmp.extend(permutations(numbers, i))
     real = [int(''.join(t)) for t in tmp]
+    print(real)
+    answer = [] 
     for r in real :
         if ch(r) :
             answer.append(r)
-
+    print(len(set(answer)))
     return len(set(answer))
