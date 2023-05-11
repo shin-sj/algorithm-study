f21a53a4d6f9cfebeb5752e72e5b1c6679c5b955
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-05-11 14:31:22 +0900

    [Silver IV] Title: 30, Time: 72 ms, Memory: 31680 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,18 @@
+#30
+import sys
+
+n = input()
+n = sorted(n, reverse=True)
+sum = 0
+
+if '0' not in n:
+    print(-1)
+else :
+    for i in n :
+        sum += int(i) 
+    if sum % 3 != 0 :
+        print(-1)
+    else :
+        print(''.join(n))
+    
+    
