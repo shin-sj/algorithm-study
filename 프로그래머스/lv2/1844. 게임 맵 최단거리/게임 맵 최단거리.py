435d4b8ea8819f1b87856e0008efa81f896c81f2
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-05-13 12:37:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -5,7 +5,8 @@ dy = [1,-1,0,0]
 def bfs(maps) :
     xend, yend = len(maps), len(maps[0])
     dQ = deque()
-    dQ.append((0, 0))
+    dQ.append((0,0))
+
     while dQ :
         x, y = dQ.popleft()
         for i in range(4) :
@@ -16,13 +17,9 @@ def bfs(maps) :
                 if maps[xx][yy] == 1 :
                     maps[xx][yy] = maps[x][y] + 1
                     dQ.append((xx, yy))
-    
+        
 def solution(maps):
-    answer = 0
-    xend, yend = len(maps), len(maps[0])
-    bfs(maps) 
-    answer = maps[xend-1][yend-1]
-    if answer == 1 :
-        return -1
-    else :
-        return answer
+    xend, yend = len(maps), len(maps[0]) #n과 m의 크기
+    bfs(maps)              
+    return maps[xend-1][yend-1] if maps[xend-1][yend-1] !=1 else -1
+    
