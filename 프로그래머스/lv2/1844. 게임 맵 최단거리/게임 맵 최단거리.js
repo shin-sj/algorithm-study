a752bb754cfb527d777d1927b64cf24f6dc31e50
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-06-15 16:44:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.88 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,27 +1,34 @@
 function solution(maps) {
-    const xEnd = maps.length;
-    const yEnd= maps[0].length;
-    const dx = [0, 0, -1, 1];
-    const dy = [-1, 1, 0, 0];
+    const answer = 0;
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
+    const xend = maps.length;
+    const yend = maps[0].length;
     
     const bfs = () => {
         const queue = [[0, 0]];
         while (queue.length) {
             const [x, y] = queue.shift();
-            for(let i = 0; i < 4; i++)
-            {
+            for(let i = 0; i < 4; i++) {
                 const xx = x + dx[i];
                 const yy = y + dy[i];
-                if (xx >= 0 && xx < xEnd && yy >= 0 && yy < yEnd && maps[xx][yy] == 1) {
-                    maps[xx][yy] = maps[x][y] + 1;
-                    queue.push([xx, yy]);
+                
+                if(xx>=0 && xx<xend && yy >=0 && y<yend) {
+                    if(maps[xx][yy]==1) {
+                        maps[xx][yy] = maps[x][y] + 1;
+                        queue.push([xx, yy]);
+                    }
                 }
+                
+                
+                
             }
         }
-    }
-
-    bfs();
     
-    if(maps[xEnd-1][yEnd-1] != 1) return maps[xEnd-1][yEnd-1];
+    
+    }
+    bfs();
+    if(maps[xend-1][yend-1] != 1) 
+        return maps[xend-1][yend-1];
     else return -1;
 }
