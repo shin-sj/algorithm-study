ed46a2d7e9f0c2c32e6289e11b82dc4240a193a2
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-06-16 19:44:47 +0900

    [level 0] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,18 +1,10 @@
 function solution(before, after) {
     var answer = 0;
-    const map = new Map();
-    for(let b of before) {
-        if(!map.has(b)) map.set(b, 1);
-        else map.set(b, map.get(b) + 1);
-    } 
-    for(let a of after) {
-        if(map.get(a) > 0) map.set(a, map.get(a)-1); 
-        else return 0;
-    }
-    for(let [k, v] of map) {
-        if(v !=0 ) return 0;
-    }
-    
-    
-    return 1;
+    const a = [...before];
+    const b = [...after];
+    a.sort();
+    b.sort();
+    if(a.join('')==b.join('')) 
+        return 1;
+    else return 0;
 }
