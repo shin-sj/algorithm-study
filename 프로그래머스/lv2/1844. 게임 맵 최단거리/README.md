f7854e48e4df7686cc0aaa2c79dfaac44651fbf5
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-06-16 11:37:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 27.41 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 30.88 ms
+메모리: 37.2 MB, 시간: 27.41 ms
 
 ### 구분
 
--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,34 +1,32 @@
 function solution(maps) {
     const answer = 0;
-    const dx = [0,0,1,-1];
-    const dy = [1,-1,0,0];
     const xend = maps.length;
     const yend = maps[0].length;
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
     
     const bfs = () => {
-        const queue = [[0, 0]];
-        while (queue.length) {
+        const queue = [[0,0]]; 
+        while(queue.length) {
             const [x, y] = queue.shift();
-            for(let i = 0; i < 4; i++) {
+            for (let i = 0; i < 4; i++) {
                 const xx = x + dx[i];
                 const yy = y + dy[i];
-                
-                if(xx>=0 && xx<xend && yy >=0 && y<yend) {
-                    if(maps[xx][yy]==1) {
-                        maps[xx][yy] = maps[x][y] + 1;
+                if (xx>=0 && xx<xend && yy>=0 && yy<yend) {
+                    if (maps[xx][yy] == 1) {
+                        maps[xx][yy] = maps[x][y]+ 1;
                         queue.push([xx, yy]);
                     }
                 }
                 
-                
-                
             }
+            
         }
-    
-    
-    }
+        
+        
+    };
     bfs();
-    if(maps[xend-1][yend-1] != 1) 
-        return maps[xend-1][yend-1];
-    else return -1;
+    if(maps[xend-1][yend-1] == 1) return -1;
+    else return maps[xend-1][yend-1];
+    
 }
