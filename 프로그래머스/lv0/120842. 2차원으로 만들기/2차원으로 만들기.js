76f0debe2de0a033b9aefc3c01b1134dc5a54686
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-06-16 12:19:11 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,9 +1,7 @@
 function solution(num_list, n) {
-    const answer = [];
-    
-    for(let i = 0; i < num_list.length / n; i++) {
-        answer.push(num_list.slice(i * n, i * n + n))
+    var answer = [];
+    for(let i=0; i<num_list.length/n; i++) {
+        answer.push(num_list.slice(i*n, i*n+n));
     }
-    
     return answer;
 }
