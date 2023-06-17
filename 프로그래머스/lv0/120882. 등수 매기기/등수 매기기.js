1fc4a967b9a46ca7e1079ad5997f17f0f03f3574
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-06-17 13:04:03 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,16 +1,10 @@
 function solution(score) {
-    var answer = [];
-    var avg = [];
-    for(let i =0; i< score.length; i++) {
-        const sum = score[i].reduce((a,b) => a+b);
-        avg.push(sum/2); 
-    }
-
-    let tmp = avg.slice().sort(function(a,b){return b-a});
-    console.log(tmp);
-    for(let i =0; i < avg.length; i++) {
-        answer.push(tmp.indexOf(avg[i])+1);
-    }
-    
-    return answer;
+    // 1. score의 평균 점수를 구한다.
+  	let avg = score.map(v=>(v[0]+v[1])/2);
+  	
+  	// 2. avg를 내림차순으로 정렬한 새로운 배열 배열 sorted를 선언한다.
+    let sorted = avg.slice().sort((a,b)=>b-a);
+  	
+  	// 3. avg의 요소와 동일한 sorted 배열의 index 값에 1을 더한다.
+    return avg.map(v => sorted.indexOf(v) + 1);
 }
